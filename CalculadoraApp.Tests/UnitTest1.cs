9efc76b67841f8b0e16219eddbf3e055f45ad9f2
AndreaDu2001 <hidalgoandrea193@gmail.com>
2025-12-09 11:04:14 -0500

    Proyecto Calcuoladora

---
+++ CalculadoraApp.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+namespace CalculadoraApp.Tests
+{
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void Sumar_DosNumeros_RetornaSumaCorrecta()
+        {
+            // Arrange
+            var calc = new CalculadoraApp.Calculadora();
+            var resultado = calc.Sumar(1, 3);
+            Assert.Equal(5, resultado);
+            
+        }
+    }
+}
+
